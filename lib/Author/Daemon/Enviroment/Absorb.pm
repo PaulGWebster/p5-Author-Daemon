bd40e0f66b91e3a0b3b0574a28f82c53e7579bbf
Paul G Webster <paul.g.webster@googlemail.com>
2022-05-07 22:43:32 +0100

    Updated base simplelog/absorb with new funcs

--- lib/Author/Daemon/Enviroment/Absorb.pm
+++ lib/Author/Daemon/Enviroment/Absorb.pm
@@ -1,5 +1,14 @@
-use strict;
-use warnings;
 package Author::Daemon::Enviroment::Absorb;
+
+use v5.28;
+use warnings;
+use strict;
+use experimental 'signatures';
+
+sub new ( $class, $env, @args ) {
+    my $self = {};
+    bless $self, $class;
+    return $self;
+}
 
 1;
--- lib/Author/Daemon/SimpleLog.pm
+++ lib/Author/Daemon/SimpleLog.pm
@@ -1,5 +1,43 @@
-use strict;
-use warnings;
 package Author::Daemon::SimpleLog;
+
+use v5.28;
+use warnings;
+use strict;
+use experimental 'signatures';
+
+sub new ( $class, $message = undef ) {
+    my $self = {
+        'message'   =>  $message,
+        'level'     =>  'INFO',
+        'levels'    =>  {
+            'CRITICAL' => 0,
+            'WARN'     => 1,
+            'INFO'     => 2,
+            'DEBUG'    => 3
+        }
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub simple ( $self, $message ) {
+    return Author::Daemon::SimpleLog->new($message);
+}
+
+sub message ( $self, $set_message = undef ) {
+    if ( defined $set_message ) {
+        $self->{'message'} = $set_message;
+        return $self;
+    }
+    return $self->{'message'};
+}
+
+sub level ( $self, $set_value = undef ) {
+    if ( $set_value && defined $self->{'levels'}->{ uc($set_value) } ) {
+        $self->{'level'} = uc($set_value);
+        return $self;
+    }
+    return $self->{'level'};
+}
 
 1;
